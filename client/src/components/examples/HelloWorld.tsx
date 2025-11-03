a62d47522a6660aa64ab347f9d0c5b6ab954c136
ravisingh8v <singhravi4v@gmail.com>
2025-11-03 17:25:17 +0530

    feat: updated

--- client/src/components/examples/HelloWorld.tsx
+++ client/src/components/examples/HelloWorld.tsx
@@ -1,6 +1,6 @@
 import HelloWorld from "../../pages/hello-world";
 
 export default function HelloWorldExample() {
-  console.log("its change");
+  console.log("its change 2");
   return <HelloWorld />;
 }
