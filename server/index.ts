dbbed7eeaca3fc35e8f489b454797e575867979f
ravisingh8v <singhravi4v@gmail.com>
2025-11-04 18:07:55 +0530

    removed

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,6 @@ import { registerRoutes } from "./routes";
 import { log, serveStatic, setupVite } from "./vite";
 
 const app = express();
-console.log("log again");
 declare module "http" {
   interface IncomingMessage {
     rawBody: unknown;
