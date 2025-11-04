6d190821277e439b5c9215b121dc9e6ce88d01a3
ravisingh8v <singhravi4v@gmail.com>
2025-11-04 17:12:00 +0530

    feat:

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,6 @@ import { registerRoutes } from "./routes";
 import { log, serveStatic, setupVite } from "./vite";
 
 const app = express();
-console.log("again added");
 declare module "http" {
   interface IncomingMessage {
     rawBody: unknown;
