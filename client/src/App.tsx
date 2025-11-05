e6e709ef8e525f887435115eaa6c64cd78486eea
ravisingh8v <singhravi4v@gmail.com>
2025-11-05 12:54:28 +0530

    added hello world component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import HelloWorld from "@/pages/hello-world";
 import NotFound from "@/pages/not-found";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Route, Switch } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import HelloWorld from "./pages/hello-world";
 
 function Router() {
   return (
